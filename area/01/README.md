9b228b8cf50ae7134d6c68e922dd417e9e6996fc
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-04-20 01:48:38 -0300

    Adiciona o arquivo area/01/.gitignore

---
+++ area/01/README.md
@@ -0,0 +1,4 @@
+Certo, vou criar uma classe Area em C++23 que calcula áreas de figuras geométricas básicas. Para isso, vou utilizar conceitos avançados de programação orientada a objetos e recursos novos do C++23, como conceitos e sobrecarga de operadores.
+
+Segue abaixo o código da classe Area:
+
---
+++ area/01/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <concepts>
+using namespace std;
+
+// Definindo um conceito para tipos que têm função área
+template <typename T>
+concept HasArea = requires(T t) {
+    { t.area() } -> std::same_as<double>;
+};
+
+// Definindo uma classe template para retângulos
+template <typename T>
+class Rectangle {
+public:
+    Rectangle(T width, T height) : width(width), height(height) {}
+
+    double area() const {
+        return static_cast<double>(width) * static_cast<double>(height);
+    }
+
+private:
+    T width, height;
+};
+
+// Definindo uma classe template para círculos
+template <typename T>
+class Circle {
+public:
+    Circle(T radius) : radius(radius) {}
+
+    double area() const {
+        return 3.14159265359 * static_cast<double>(radius) * static_cast<double>(radius);
+    }
+
+private:
+    T radius;
+};
+
+// Definindo a classe Area
+class Area {
+public:
+    // Sobrecarga do operador () para calcular a área de um objeto que tem função área
+    template <HasArea T>
+    double operator()(T const& t) const {
+        return t.area();
+    }
+
+    // Sobrecarga do operador + para somar áreas
+    double operator+(double const& a, double const& b) const {
+        return a + b;
+    }
+
+    // Sobrecarga do operador + para somar áreas de objetos que têm função área
+    template <HasArea T, HasArea U>
+    double operator+(T const& t, U const& u) const {
+        return t.area() + u.area();
+    }
+};
+
+int main() {
+    // Criando objetos de retângulo e círculo
+    Rectangle<int> retangulo(5, 3);
+    Circle<double> circulo(2.5);
+
+    // Criando um objeto da classe Area
+    Area area;
+
+    // Calculando a área do retângulo e do círculo
+    double areaRetangulo = area(retangulo);
+    double areaCirculo = area(circulo);
+
+    // Somando as áreas do retângulo e do círculo
+    double areaTotal = areaRetangulo + areaCirculo;
+
+    // Imprimindo a área total na tela
+    cout << "A area total e: " << areaTotal << endl;
+
+    return 0;
+}
