6650d17d3c8b1be40be664985e45628c05aa400c
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-05-20 23:42:30 -0300

    Atualiza o arquivo a_tour_cpp/01_exemplo.cpp

--- a_tour_cpp/01_exemplo.cpp
+++ a_tour_cpp/01_exemplo.cpp
@@ -1,5 +1,6 @@
 /* hello.cpp: A standard "Hello, world!" program */
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -9,7 +10,13 @@ using namespace std;
 
 int main()
 {
-    cout << "hello, world" << endl;
+    auto b = true;     // a bool
+    auto ch = 'x' ;    // a char
+    auto i = 123;      // an int
+    auto d = 1.2;      // a double
+    auto y = 9.9;      // z has the type of whatever sqrt(y) returns
+    auto z = sqrt (y); // z has the type of whatever sqrt(y) returns
+    auto bb {true};    // bb is a bool
 
     return 0; // success return
 }
