2fd45348dcb9b73eeb3583e33c4ce2a38653786c
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-05-22 22:42:46 -0300

    Atualiza o arquivo a_tour_cpp/02_exemplo.cpp

--- a_tour_cpp/02_exemplo.cpp
+++ a_tour_cpp/02_exemplo.cpp
@@ -1,5 +1,7 @@
 /* hello.cpp: A standard "Hello, world!" program */
 #include <iostream>
+#include <complex>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +11,15 @@ using namespace std;
 
 int main()
 {
-    cout << "hello, world" << endl;
+    double d1 = 2.3;                  // initialize d1 to 2.3
+    double d2 {2.3};                  // initialize d2 to 2.3
+    double d3 = {2.3};                // initialize d3 to 2.3 (the = is optional with { ... })
+
+    complex<double> z = 1;            // a complex number with double-precision floating-point scalars
+    complex<double> z2 {d1, d2};
+    complex<double> z3 = {d1, d2};    // the = is optional with { ... }
+
+    vector<int> v {1, 2, 3, 4, 5, 6}; // a vector of ints
 
     return 0; // success return
 }
