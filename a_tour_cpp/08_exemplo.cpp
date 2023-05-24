fdecd13705f400db306fad523b2ed43635aa6921
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-05-24 01:18:17 -0300

    Adiciona o arquivo a_tour_cpp/08_exemplo.cpp

--- a_tour_cpp/08_exemplo.cpp
+++ a_tour_cpp/08_exemplo.cpp
@@ -19,15 +19,6 @@ void printValuesByPointer (const int* v)
     std::cout << std::endl;
 }
 
-void printValuesByPointer2 (const int* v)
-{
-    for (auto i = 0; i < 10; i++)
-    {
-        std::cout << * (v + i) << " ";
-    }
-    std::cout << std::endl;
-}
-
 int main()
 {
     int v[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -39,9 +30,6 @@ int main()
     // Imprimir valores usando ponteiros
     std::cout << "Valores do vetor (ponteiros): ";
     printValuesByPointer (v);
-
-    std::cout << "Valores do vetor (ponteiros2): ";
-    printValuesByPointer2 (v);
 
     return 0;
 }
