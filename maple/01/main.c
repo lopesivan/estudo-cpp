e361dae77bc9e79589e6cbf2a455808eeebb10d3
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-04-26 04:03:52 -0300

    Adiciona o arquivo maple/01/README.md

--- maple/01/main.c
+++ maple/01/main.c
@@ -1,32 +1,35 @@
-#include <stdio.h>
 #include <dlfcn.h>
+#include <stdio.h>
 
 int main() {
-    // carrega a biblioteca compartilhada
-    void* handle = dlopen("/opt/maple2021/bin.X86_64_LINUX/libmaple.so", RTLD_NOW);
-    if (!handle) {
-        printf("Erro ao carregar a biblioteca compartilhada: %s\n", dlerror());
-        return 1;
-    }
+  // carrega a biblioteca compartilhada
+  void *handle =
+      dlopen("/opt/maple2021/bin.X86_64_LINUX/libmaple.so", RTLD_NOW);
+  if (!handle) {
+    printf("Erro ao carregar a biblioteca compartilhada: %s\n", dlerror());
+    return 1;
+  }
 
-    // obtem o ponteiro para a função "sqrt" da biblioteca
-    double (*sqrt)(double) = dlsym(handle, "_Z4sqrtd");
+  // obtem o ponteiro para a função "sqrt" da biblioteca
+  // double (*sqrt) (double) = dlsym (handle, "_Z4sqrtd");
+  // correção:
+  double (*sqrt)(double) = dlsym(handle, "hfu_sqrt");
 
-    // verifica se houve algum erro ao obter a função
-    char* error = dlerror();
-    if (error != NULL) {
-        printf("Erro ao obter a função: %s\n", error);
-        dlclose(handle);
-        return 1;
-    }
-
-    // chama a função "sqrt"
-    double numero = 16.0;
-    double resultado = sqrt(numero);
-    printf("A raiz quadrada de %f é %f\n", numero, resultado);
-
-    // fecha a biblioteca compartilhada
+  // verifica se houve algum erro ao obter a função
+  char *error = dlerror();
+  if (error != NULL) {
+    printf("Erro ao obter a função: %s\n", error);
     dlclose(handle);
+    return 1;
+  }
 
-    return 0;
+  // chama a função "sqrt"
+  double numero = 16.0;
+  double resultado = sqrt(numero);
+  printf("A raiz quadrada de %f é %f\n", numero, resultado);
+
+  // fecha a biblioteca compartilhada
+  dlclose(handle);
+
+  return 0;
 }
