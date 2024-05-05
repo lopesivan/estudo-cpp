c0ddde5d64f36b977059c28ea2f107f51665b1e6
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-05-05 01:11:40 -0300

    Atualiza o arquivo box2d/05/build.lua

--- box2d/05/build.lua
+++ box2d/05/build.lua
@@ -1,6 +1,8 @@
 local overseer = require("overseer")
 overseer.run_template({ name = "make" }, function(task)
-    if task then
-        overseer.run_action(task, "open float")
-    end
+	if task then
+		overseer.run_action(task, "open float")
+	end
 end)
+
+-- abro com o nvim e uso <space><Enter>, para executar o make
