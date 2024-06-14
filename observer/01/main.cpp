dd8b5ca10922b465192b44a85a785fd27b15e41c
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-06-14 13:44:02 -0300

    Atualiza o arquivo observer/01/main.cpp

--- observer/01/main.cpp
+++ observer/01/main.cpp
@@ -1,72 +1,72 @@
-
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-
 // Observer interface
-class Observer
-{
+class Observer {
 public:
-    virtual void update (const string& message) = 0;
+    virtual void update(const string& message) = 0;
+    virtual ~Observer() = default;
 };
 
 // Concrete Observer
-class ConcreteObserver : public Observer
-{
+class ConcreteObserver : public Observer {
 public:
-    void update (const string& message) override
-    {
+    void update(const string& message) override {
         cout << "Received message: " << message << endl;
     }
 };
 
 // Publisher (Subject) class
-class Publisher
-{
+class Publisher {
 private:
     vector<Observer*> observers;
 
 public:
     // Attach an observer
-    void subscribe (Observer* observer)
-    {
-        observers.push_back (observer);
+    void subscribe(Observer* observer) {
+        observers.push_back(observer);
     }
 
     // Detach an observer
-    void unsubscribe (Observer* observer)
-    {
-        // remove observer from the vector
+    void unsubscribe(Observer* observer) {
+        auto it = find(observers.begin(), observers.end(), observer);
+        if (it != observers.end()) {
+            observers.erase(it);
+        }
     }
 
     // Notify all observers
-    void notify (const string& message)
-    {
-        for (Observer* observer : observers)
-        {
-            observer->update (message);
+    void notify(const string& message) {
+        for (Observer* observer : observers) {
+            observer->update(message);
         }
+    }
+
+    // Publish a message
+    void publish(const string& message) {
+        notify(message);
     }
 };
 
-int main()
-{
+int main() {
     Publisher publisher;
 
     // Create subscribers
-    ConcreteSubscriber subscriber1;
-    ConcreteSubscriber subscriber2;
+    ConcreteObserver subscriber1;
+    ConcreteObserver subscriber2;
 
     // Attach subscribers to the publisher
-    publisher.subscribe (&subscriber1);
-    publisher.subscribe (&subscriber2);
+    publisher.subscribe(&subscriber1);
+    publisher.subscribe(&subscriber2);
 
     // Publish messages
-    publisher.publish ("Message 1");
-    publisher.publish ("Message 2");
+    publisher.publish("Message 1");
+    publisher.publish("Message 2");
 
     return 0;
 }
+
