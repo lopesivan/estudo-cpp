0685545ef6d4de38f217036670ee6a1cb412dffd
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-05-18 06:24:02 -0300

    Adiciona o arquivo opengl/01/.clang-format

--- opengl/01/main.cpp
+++ opengl/01/main.cpp
@@ -3,24 +3,23 @@
 
 void display()
 {
-    glClear (GL_COLOR_BUFFER_BIT);
-    glColor3f (1.0, 1.0, 1.0);
-    glBegin (GL_LINE_LOOP);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_LINE_LOOP);
     for (int i = 0; i < 360; i++)
     {
         float rad = i * M_PI / 180.0;
-        glVertex2f (cos (rad), sin (rad));
+        glVertex2f(cos(rad), sin(rad));
     }
     glEnd();
     glFlush();
 }
 
-int main (int argc, char **argv)
+int main(int argc, char** argv)
 {
-    glutInit (&argc, argv);
-    glutCreateWindow ("Circle");
-    glutDisplayFunc (display);
+    glutInit(&argc, argv);
+    glutCreateWindow("Circle");
+    glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
-
