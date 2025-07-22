c279b2ac2fd9a8596693cff98d3649a903c9c057
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-07-22 03:45:33 -0300

    Remove o arquivo "wxWidgets/01/Existe@std.range@?.md"

--- wxWidgets/01/main.cpp
+++
@@ -1,7 +0,0 @@
-import math_utils;
-#include <iostream>
-
-int main()
-{
-    std::cout << soma (2, 3) << "\n";
-}
--- wxWidgets/01/math_utils.ixx
+++
@@ -1,6 +0,0 @@
-export module math_utils;
-
-export int soma (int a, int b)
-{
-    return a + b;
-}
