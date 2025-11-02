fb4d9fbe642d031ada9136ef941b7eabde2afa28
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-02 13:54:02 -0300

    :add: Adiciona o arquivo cmake/01/src/main.cpp

---
+++ cmake/01/src/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include "yaml-cpp/yaml.h"
+
+using namespace std;
+int main()
+{
+    string name = "Guest";
+
+    YAML::Node config = YAML::LoadFile ("etc/config.yaml");
+    if (config["name"])
+        name = config["name"].as<string>();
+
+    cout << "Welcome " << name << endl;
+    return 0;
+}
