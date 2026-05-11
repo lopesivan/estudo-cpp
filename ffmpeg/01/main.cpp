3c5aefc63104038f4f49f4c881d5851ef3c739cd
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-05-11 16:33:14 -0300

    :style: Atualiza o estilo do arquivo ffmpeg/01/main.cpp

--- ffmpeg/01/main.cpp
+++ ffmpeg/01/main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     FFmpegJob job;
-    job.input = "entrada.mp3";
-    job.output = "saida.ogg";
-    job.audioCodec = "libvorbis";
+    job.input         = "file_example_MP3_5MG.mp3";
+    job.output        = "file_example_MP3_5MG.ogg";
+    job.audioCodec    = "libvorbis";
     job.audioBitrateK = 192;
 
     UnixCommand cmd = make_ffmpeg_command (job);
