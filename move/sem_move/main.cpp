405952cf2b92f14add87e886c56c04bd21cf2252
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-05-12 00:23:42 -0300

    :style: Atualiza o estilo do arquivo move/sem_move/main.cpp

--- move/sem_move/main.cpp
+++ move/sem_move/main.cpp
@@ -6,7 +6,7 @@ int main()
 
     std::string a = "hello";
 
-    std::string b = std::move(a);
+    std::string b = a;
 
     std::cout << "a = " << a << '\n';
     std::cout << "b = " << b << '\n';
