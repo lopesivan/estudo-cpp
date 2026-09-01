00fec95805c0284e16a44fca77c4d2a350fdc0d8
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-08-31 21:45:47 -0300

    fix: atualiza algebra_linear/01-ex.cpp

--- algebra_linear/01-ex.cpp
+++ algebra_linear/01-ex.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
-#include <numeric>   // inner_product
+#include <numeric>  // inner_product
 #include <vector>
-using namespace std;
 
-int dotProduct(const vector<int>& vec1, const vector<int>& vec2) {
-    if (vec1.size() != vec2.size()) {
-        cout << "Erro: os vetores precisam ter o mesmo tamanho" << endl;
+int dotProduct(const std::vector<int>& vec1,
+               const std::vector<int>& vec2)
+{
+    if(vec1.size() != vec2.size())
+    {
+        std::cout << "Erro: os vetores precisam ter o mesmo tamanho"
+                  << std::endl;
         return 0;
     }
-    return inner_product(vec1.begin(), vec1.end(), vec2.begin(), 0);
+    return std::inner_product(
+        vec1.begin(), vec1.end(), vec2.begin(), 0);
 }
 
-int main() {
-    vector<int> vec1 = {1, 2, 3};
-    vector<int> vec2 = {4, 5, 6};
+int main()
+{
+    std::vector<int> vec1 = {1, 2, 3};
+    std::vector<int> vec2 = {4, 5, 6};
 
     int result = dotProduct(vec1, vec2);
-    cout << "Produto escalar dos vetores: " << result << endl;
+    std::cout << "Produto escalar dos vetores: " << result
+              << std::endl;
     return 0;
 }
