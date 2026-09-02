f1db3a6a4fb7ca15aef9a27283989404592684ae
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-09-02 04:17:40 -0300

    fix: atualiza template/tempo_de_compilacao/01/dobro.cpp

--- template/tempo_de_compilacao/01/dobro.cpp
+++ template/tempo_de_compilacao/01/dobro.cpp
@@ -13,6 +13,7 @@ int main()
     std::cout << "Dobro de 5: " << Dobro<5>::valor << std::endl;    // 10
     std::cout << "Dobro de 10: " << Dobro<10>::valor << std::endl;  // 20
     std::cout << "Dobro de 7: " << Dobro<7>::valor << std::endl;    // 14
+    std::cout << "(Dobro de 7)*2: " << 2*Dobro<7>::valor << std::endl;    // 14
 
     return 0;
 }
