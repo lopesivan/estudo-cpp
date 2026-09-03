2de09439b6a15778533ed2cbe20fb0b699ec709d
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-09-03 03:54:22 -0300

    fix: atualiza ngspice/03-ngspice-47-compiled/RC/cpp/02/plot.py

--- ngspice/03-ngspice-47-compiled/RC/cpp/02/plot.py
+++ ngspice/03-ngspice-47-compiled/RC/cpp/02/plot.py
@@ -7,20 +7,8 @@ t = data[:, 0]
 vin = data[:, 1]
 vout = data[:, 2]
 
-Ain = (vin.max() - vin.min()) / 2
-Aout = (vout.max() - vout.min()) / 2
-
-plt.plot(
-    t * 1e3,
-    vin,
-    label=f"v(in)  A={Ain:.3f} V"
-)
-
-plt.plot(
-    t * 1e3,
-    vout,
-    label=f"v(out) A={Aout:.3f} V"
-)
+plt.plot(t * 1e3, vin, label="v(in)")
+plt.plot(t * 1e3, vout, label="v(out)")
 
 plt.xlabel("Tempo [ms]")
 plt.ylabel("Tensão [V]")
